8faf8e79d57bbeb575624d90a6dd3a370deff882
FP Onyx <118073602+EfrenFloresPorras@users.noreply.github.com>
2024-04-22 17:14:44 -0600

    This time we changed the search files from .ipynb to .py to be used in the interface

---
+++ buscaCaminosSolver.py
@@ -0,0 +1,173 @@
+import heapq
+import time
+
+grid = [
+    ['A', 'B', 'C', 'D', 'E'],
+    ['F', 'G', 'H', 'I', 'J'],
+    ['K', 'L', 'M', 'N', 'Ñ'],
+    ['O', 'P', 'Q', 'R', 'S'],
+    ['T', 'U', 'V', 'W', 'X']
+]
+
+# Convertimos los puntos de inicio y fin a coordenadas
+# Definimos la posición de cada coordenada
+locations = {
+    'A': (0, 0), 'B': (0, 1), 'C': (0, 2), 'D': (0, 3), 'E': (0, 4),
+    'F': (1, 0), 'G': (1, 1), 'H': (1, 2), 'I': (1, 3), 'J': (1, 4),
+    'K': (2, 0), 'L': (2, 1), 'M': (2, 2), 'N': (2, 3), 'Ñ': (2, 4),
+    'O': (3, 0), 'P': (3, 1), 'Q': (3, 2), 'R': (3, 3), 'S': (3, 4),
+    'T': (4, 0), 'U': (4, 1), 'V': (4, 2), 'W': (4, 3), 'X': (4, 4)
+}
+
+# Función para calcular la distancia de Manhattan entre dos puntos
+def manhattan_distance(start, goal):
+    dx = abs(start[0] - goal[0])  # Absoluto
+    dy = abs(start[1] - goal[1])
+    return dx + dy
+
+# Función para obtener los vecinos de un nodo en el grid
+def get_neighbors(node, grid, obstacle=None):
+    neighbors = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Arriba, Abajo, Izquierda, Derecha
+    for direction in directions:
+        neighbor = (node[0] + direction[0], node[1] + direction[1])
+        # Si los nodos se encuentran en las esquinas, no agregues vecinos fuera del grid
+        if 0 <= neighbor[0] < len(grid) and 0 <= neighbor[1] < len(grid[0]):
+            # Si el vecino es el obstáculo, saltarlo
+            if obstacle and neighbor == locations[obstacle]:
+                continue
+            neighbors.append(neighbor)
+    return neighbors
+
+# ALGORITMOS
+
+## A* Algorithm
+def astar(grid, start, goal, obstacle=None):
+    open_set = {start: (0, manhattan_distance(start, goal))} # Costo heurístico
+    came_from = {}
+    g_score = {start: 0} # Costo real
+    moves = 0
+
+    while open_set:
+        # Escoger el nodo con el menor precio
+        current = min(open_set, key=lambda x: open_set[x][1])
+
+        if current == goal:
+            # Reconstruir camino
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            # Devoler camino ordenado, costo y número de movimientos
+            return path[::-1], g_score[goal], moves
+
+        del open_set[current]
+
+        for neighbor in get_neighbors(current, grid, obstacle):
+            # Costo al moverse una cuadrícula
+            tentative_g_score = g_score[current] + 1
+            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score = tentative_g_score + manhattan_distance(neighbor, goal)
+                open_set[neighbor] = (tentative_g_score, f_score)
+                moves += 1
+    return "No se encontró un camino", 0, 0
+
+## Beam Search Algorithm
+def beamSearch(grid, start, goal, k, obstacle=None):
+    open_set = {start: (0, manhattan_distance(start, goal))} # Heuristic cost
+    came_from = {}
+    g_score = {start: 0} # Actual cost
+    moves = 0
+
+    while open_set:
+        # Choose the node with the lowest cost
+        current = min(open_set, key=lambda x: open_set[x][1])
+
+        if current == goal:
+            # Reconstruct path
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            # Return ordered path, cost, and number of moves
+            return path[::-1], g_score[goal], moves
+
+        del open_set[current]
+
+        for neighbor in get_neighbors(current, grid, obstacle):
+            # Cost to move one grid
+            tentative_g_score = g_score[current] + 1
+            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score = tentative_g_score + manhattan_distance(neighbor, goal)
+                open_set[neighbor] = (tentative_g_score, f_score)
+                moves += 1
+        # Beam Search
+        if len(open_set) > k:
+            open_set = dict(heapq.nsmallest(k, open_set.items(), key=lambda x: x[1][1]))
+    return "No path found", 0, 0
+
+# Función para imprimir el camino encontrado
+def print_path(path):
+    for node in path:
+        for key, value in locations.items():
+            if value == node:
+                print(key, end=' -> ')
+                break
+
+## ------------------------------ A* Algorithm Solving Example ------------------------------ ##
+# Seleccionar nodos y obstáculo
+start_letter = 'O'
+goal_letter = 'I'
+obstacle_letter = 'H'
+
+start = locations[start_letter]
+goal = locations[goal_letter]
+
+# Verificar si se ingresó un obstáculo
+if obstacle_letter == '':
+    obstacle = None
+else:
+    obstacle = obstacle_letter
+
+# Calcular tiempo
+start_time = time.perf_counter_ns()
+path, cost, moves = astar(grid, start, goal, obstacle)
+astarSolverTime = (time.perf_counter_ns() - start_time) * 1e-9
+
+print("Camino encontrado: ")
+print_path(path)
+print(f"Costo total: {cost}")
+print(f"Número de movimientos: {moves}")
+print(f"Tiempo de ejecución: {astarSolverTime}s")
+
+## ------------------------------ Beam Search Algorithm Solving Example ------------------------------ ##
+# Seleccionar nodos y obstáculo
+start_letter = 'O'
+goal_letter = 'I'
+obstacle_letter = 'H'
+
+start = locations[start_letter]
+goal = locations[goal_letter]
+
+# Verificar si se ingresó un obstáculo
+if obstacle_letter == '':
+    obstacle = None
+else:
+    obstacle = obstacle_letter
+
+# Calcular tiempo
+start_time = time.perf_counter_ns()
+path, cost, moves = beamSearch(grid, start, goal, 13, obstacle)
+astarSolverTime = (time.perf_counter_ns() - start_time) * 1e-9
+
+print("Camino encontrado: ")
+print_path(path)
+print(f"Costo total: {cost}")
+print(f"Número de movimientos: {moves}")
+print(f"Tiempo de ejecución: {astarSolverTime}s")
---
+++ rompecocosSolver.py
@@ -0,0 +1,135 @@
+import heapq
+import time
+
+# Obtener distancia Manhattan (heurístca)
+
+def heuristica(puzzleState):
+    distanciaTotal = 0
+    for i in range(3): # Fila
+        for j in range(3): # Columna
+
+            # Si la pieza no es hueco, calcular la fila y columna objetivo de cada número
+            if puzzleState[i][j] != 0:
+                filaObjetivo = (puzzleState[i][j] - 1) // 3
+                columnaObjetivo = (puzzleState[i][j] - 1) % 3
+                distanciaTotal += abs(i - filaObjetivo) + abs(j - columnaObjetivo)
+    return distanciaTotal
+
+# Obtener posición vacía del tablero
+
+def obtenerHueco(puzzleState):
+    for i in range(3):
+        for j in range(3):
+            # Si encuentra 0 (hueco), devolver posición del hueco
+            if puzzleState[i][j] == 0:
+                return i, j
+            
+# Obtener sucesores y verificar movimientos del hueco
+
+def obtenerSucesores(puzzleState):
+    sucesores = []
+    filaV, columnaV = obtenerHueco(puzzleState) #Fila vacía, columna vacía
+    movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)] # Derecha, izquierda, abajo, arriba
+    
+    for df, dc in movimientos:
+        newFila, newColumna = filaV + df, columnaV + dc
+        # Verificar que los movimientos no salgan del tablero y sean horizontales o verticales
+        if 0 <= newFila < 3 and 0 <= newColumna < 3:
+            newState = [fila[:] for fila in puzzleState] # Copia del tablero actual
+            newState[filaV][columnaV], newState[newFila][newColumna] = newState[newFila][newColumna], newState[filaV][columnaV]
+            sucesores.append((newState, (newFila, newColumna)))
+    
+    return sucesores
+
+# Resolver Puzzle
+
+## Algoritmo A*
+def astarSolver(initialState):
+    currentState = initialState
+    priorityQueue = [(heuristica(initialState), initialState)]
+    visited = set()
+    visitedStates = []
+    solution = None
+
+    while priorityQueue:
+        # Extraer estado del tablero con menor heurística
+        h, currentState = heapq.heappop(priorityQueue)
+        # Agregar estados a visited y visitedStates
+        visited.add(tuple(map(tuple, currentState)))
+        visitedStates.append([fila[:] for fila in currentState])
+
+        # Si se encuentra la solución, actualizar solución
+        if currentState == [[1, 2, 3], [4, 5, 6], [7, 8, 0]]:
+            solution = currentState
+            break
+        successors = obtenerSucesores(currentState)
+        for child, _ in successors:
+            # Si no se ha explorado el estado actual, agregar a la cola de prioridad
+            if tuple(map(tuple, child)) not in visited:
+                heapq.heappush(priorityQueue, (obtenerSucesores(child), child))
+
+    # Devolver todos los movimientos realizados y la solución
+    return visitedStates, solution
+
+## Algoritmo Beam 
+def beamSearch(initialState, beamWidth):
+    currentState = [(heuristica(initialState), initialState)]
+    visited = set()
+    visitedStates = []
+    solution = None
+
+    while currentState:
+        allSuccessors = []
+        for h, state in currentState:
+            visited.add(tuple(map(tuple, state)))
+            visitedStates.append([fila[:] for fila in state])
+            successors = [(heuristica(child), child) for child, _ in obtenerSucesores(state) if tuple(map(tuple, child)) not in visited]
+            allSuccessors.extend(successors)
+
+        allSuccessors.sort(key=lambda x: x[0])
+        currentState = allSuccessors[:beamWidth]
+
+        for _, state in currentState:
+            if state == [[1, 2, 3], [4, 5, 6], [7, 8, 0]]:
+                solution = state
+                break
+
+        if solution:
+            break
+
+    return visitedStates, solution
+
+# Imprimir cada movimiento
+
+def mostrarMovimientos(visitedStates):
+    for movimiento, estado in enumerate(visitedStates, start=1):
+        print(f"--- Movimiento {movimiento} ---")
+        for fila in estado:
+            print(" ".join(map(str, fila)))
+        print()
+
+tableroInicial = [
+    [3, 1, 2],
+    [4, 0, 5],
+    [6, 7, 8]
+]
+
+print("---- A* Algorithm ---- ")
+start_time = time.perf_counter_ns()      # Agregamos 'start_time' para medir el tiempo de ejecución por cada algoritmo, se repitira esta línea por cada algoritmo
+movimientos, solucion = astarSolver(tableroInicial)
+astarSolver_time = (time.perf_counter_ns() - start_time)*1e-9     # Current time - start time = tiempo de ejecución
+mostrarMovimientos(movimientos)
+print(f"Solución en {len(movimientos)} movimientos:         Tiempo de ejecución: {astarSolver_time} s")
+
+for fila in solucion:
+    print(" ".join(map(str, fila)))
+
+print("\n\n ---- Beam Search Algorithm: ---- ")
+start_time = time.perf_counter_ns()
+movimientos, solucion = beamSearch(tableroInicial, 10)
+beamSearch_time = (time.perf_counter_ns() - start_time)*1e-9
+mostrarMovimientos(movimientos)
+print(f"Solución en {len(movimientos)} movimientos:         Tiempo de ejecución: {beamSearch_time} s")
+
+for fila in solucion:
+    print(" ".join(map(str, fila)))
